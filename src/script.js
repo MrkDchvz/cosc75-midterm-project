8d2825f32dc279a715de5121a75cb2117fab153b
Markufu ‎ <madechavez22@gmail.com>
2024-10-27 23:04:58 +0800

    Finished typing animation on header

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
+gsap.registerPlugin(TextPlugin);
+
+// Hamburget Animation + Mobile nav animation
 const hamburger = document.getElementById("hamburger");
 let showNavAnimation = gsap.timeline({paused: true});
 
-showNavAnimation.to("#mobile-nav", {duration : 0.2, left: "-2%" });
+showNavAnimation.to("#mobile-nav", {duration : 0.2, left: "0"});
 showNavAnimation.to("#mobile-nav__list li", {stagger: 0.1, x: "0%", opacity: "1"},"-=0.30");
 showNavAnimation.to("#theme-symbol", {duration: 0.2, opacity: 1},"-=0.30");
 
@@ -39,3 +42,45 @@ themeButtons.forEach((themeButton) =>  {
   });
 })
 
+// Text Animations
+const heroHeader = document.getElementById("hero-header");
+const caret = document.getElementById("caret");
+
+
+function caretAnimationStart() {
+  let tl = gsap.timeline();
+  tl.set(caret, {opacity: 1});
+  tl.to(caret, {opacity: 1, duration: 0.9});
+  tl.set(caret, {opacity: 0});
+  tl.to(caret, {opacity: 0, duration: 0.3});
+  return tl
+}
+
+function caretAnimationMiddle() {
+  let tl = gsap.timeline();
+  let offsetX = 3;
+   tl.to(heroHeader, 
+    {text: {
+      value: "Mark Adrian Dechavez",
+      duration: 1,
+    },
+    onComplete: () => {
+      gsap.set(caret, {x: `${heroHeader.offsetWidth - caret.getBoundingClientRect().left + offsetX}px`});
+      console.log(heroHeader.offsetWidth);
+    }
+  })
+  return tl;
+}
+
+
+let tlHero = gsap.timeline();
+
+
+
+tlHero
+  .add(caretAnimationStart())
+  .add(caretAnimationMiddle())
+  .add(caretAnimationStart().repeat(-1))
+
+
+tlHero.play();
