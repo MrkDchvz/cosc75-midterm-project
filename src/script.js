28e319e4e20dd025a40c9eb19247c32864d30f97
Markufu ‎ <madechavez22@gmail.com>
2024-10-30 14:43:23 +0800

    Added Ico

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ const hamburger = document.getElementById("hamburger");
 let showNavAnimation = gsap.timeline({paused: true});
 
 showNavAnimation.to("#mobile-nav", {duration : 0.2, left: "0"});
-showNavAnimation.to("#mobile-nav__list li", {stagger: 0.1, x: "0%", opacity: "1"},"-=0.30");
+showNavAnimation.to("#mobile-nav__list li", {stagger: 0.1, x: "0%", opacity: "1"},"-=0.25");
 showNavAnimation.to("#theme-symbol", {duration: 0.2, opacity: 1},"-=0.30");
 
 let isActive = false;
@@ -47,26 +47,21 @@ const heroHeader = document.getElementById("hero-header");
 const caret = document.getElementById("caret");
 
 
-function caretAnimationStart() {
+function blinkCaret() {
   let tl = gsap.timeline();
   tl.set(caret, {opacity: 1});
   tl.to(caret, {opacity: 1, duration: 0.9});
   tl.set(caret, {opacity: 0});
-  tl.to(caret, {opacity: 0, duration: 0.3});
+  tl.to(caret, {opacity: 0, duration: .45});
   return tl
 }
 
-function caretAnimationMiddle() {
+function heroTextAnimation() {
   let tl = gsap.timeline();
-  let offsetX = 3;
    tl.to(heroHeader, 
     {text: {
-      value: "Mark Adrian Dechavez",
+      value: "Mark Dechavez",
       duration: 1,
-    },
-    onComplete: () => {
-      gsap.set(caret, {x: `${heroHeader.offsetWidth - caret.getBoundingClientRect().left + offsetX}px`});
-      console.log(heroHeader.offsetWidth);
     }
   })
   return tl;
@@ -78,9 +73,7 @@ let tlHero = gsap.timeline();
 
 
 tlHero
-  .add(caretAnimationStart())
-  .add(caretAnimationMiddle())
-  .add(caretAnimationStart().repeat(-1))
+  .add(blinkCaret().repeat(-1))
+  .add(heroTextAnimation(), "=-.3")
 
 
-tlHero.play();
