a7a6776508e1ec2fe0959c57c3b2d050c7bede19
Markufu ‎ <madechavez22@gmail.com>
2024-10-30 22:35:13 +0800

    Removed hamburger nav

--- script/script.js
+++ script/script.js
@@ -1,115 +1,80 @@
-import { horizontalLoop } from "./helper.js";
-
-
+import { multiToggleClassList } from "./helper.js";
 gsap.registerPlugin(TextPlugin);
 gsap.registerPlugin(ScrollTrigger);
 
-// Hamburget Animation + Mobile nav animation
-const hamburger = document.getElementById("hamburger");
-let showNavAnimation = gsap.timeline({paused: true});
-
-showNavAnimation.to("#mobile-nav", {duration : 0.2, left: "0"});
-showNavAnimation.to("#mobile-nav__list li", {stagger: 0.1, x: "0%", opacity: "1"},"-=0.25");
-showNavAnimation.to("#theme-symbol", {duration: 0.2, opacity: 1},"-=0.30");
-
-let isActive = false;
-hamburger.addEventListener("click", () => {
-  if (!isActive) {
-    showNavAnimation.restart();
-    isActive = true;
-  } else {
-    showNavAnimation.reverse(2);
-    isActive = false;
-  }
-  hamburger.classList.toggle("is-active");
-});
-
-// For Theme Switching
-
-function toggle(element, ...args) {
-  args.forEach((arg) => {
-    element.classList.toggle(arg);
-  })
-
-}
-
 const themeButtons = document.querySelectorAll(".theme-button");
-themeButtons.forEach((themeButton) =>  {
-  // Toggle dark theme or light theme 
-  themeButton.addEventListener("click",() => {
-    themeButton.dataset.theme == "dark" ? document.documentElement.classList.toggle("light-theme") : document.documentElement.classList.toggle("dark-theme");
-    themeButton.dataset.theme == "dark" ? "light" : "dark";
+themeButtons.forEach((themeButton) => {
+  // Toggle dark theme or light theme
+  themeButton.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
     // Change the appearance of theme Icon to its corresponding theme
-    const themeIcon = themeButton.childNodes[0];
-    toggle(themeIcon, "fa-moon", "fa-sun");
-    
+    const themeIcon = document.querySelector(".theme-symbol");
+    multiToggleClassList(themeIcon, "fa-moon", "fa-sun");
   });
-})
+});
 
 // Text Animations
 const heroHeader = document.getElementById("hero-header");
 const caret = document.getElementById("caret");
 
-
 function blinkCaret() {
   let tl = gsap.timeline();
-  tl.set(caret, {opacity: 1});
-  tl.to(caret, {opacity: 1, duration: 0.9});
-  tl.set(caret, {opacity: 0});
-  tl.to(caret, {opacity: 0, duration: .45});
-  return tl
+  tl.set(caret, { opacity: 1 });
+  tl.to(caret, { opacity: 1, duration: 0.9 });
+  tl.set(caret, { opacity: 0 });
+  tl.to(caret, { opacity: 0, duration: 0.45 });
+  return tl;
 }
 
 function heroTextAnimation() {
   let tl = gsap.timeline();
-   tl.to(heroHeader, 
-    {text: {
+  tl.to(heroHeader, {
+    text: {
       value: "Mark Dechavez",
       duration: 1,
-    }
-  })
+    },
+  });
   return tl;
 }
 
-
 let tlHero = gsap.timeline();
 
-
-
-tlHero
-  .add(blinkCaret().repeat(-1))
-  .add(heroTextAnimation(), "=-.3")
+tlHero.add(blinkCaret().repeat(-1)).add(heroTextAnimation(), "=-.3");
 
 // Terminal Animation
 function TerminalDetails() {
   let tl = gsap.timeline();
-  tl.from("#terminal__statement--1 .input-statement", {text : {value: "" ,duration: 1}})
-    .set("#terminal__statement--1 .output-statement", {opacity: 1})
-    .from("#terminal__statement--2 .input-statement", {text : {value: "" ,duration: 4}})
-    .set("#terminal__statement--2 .output-statement", {opacity: 1})
-    .from("#terminal__statement--3 .input-statement", {text : {value: "" ,duration: 2}})
-    .set("#terminal__statement--3 .output-statement", {opacity: 1})
-    .from("#terminal__statement--4 .input-statement", {text : {value: "" ,duration: 1}})
-    .set("#terminal__statement--4 .output-statement", {opacity: 1})
-    .to("#terminal__statement--5 .input-statement", {opacity: 1})
+  tl.from("#terminal__statement--1 .input-statement", {
+    text: { value: "", duration: 1 },
+  })
+    .set("#terminal__statement--1 .output-statement", { opacity: 1 })
+    .from("#terminal__statement--2 .input-statement", {
+      text: { value: "", duration: 4 },
+    })
+    .set("#terminal__statement--2 .output-statement", { opacity: 1 })
+    .from("#terminal__statement--3 .input-statement", {
+      text: { value: "", duration: 2 },
+    })
+    .set("#terminal__statement--3 .output-statement", { opacity: 1 })
+    .from("#terminal__statement--4 .input-statement", {
+      text: { value: "", duration: 1 },
+    })
+    .set("#terminal__statement--4 .output-statement", { opacity: 1 })
+    .to("#terminal__statement--5 .input-statement", { opacity: 1 });
 
-    return tl
-
-  }
-
-
+  return tl;
+}
 
 let tlTerminal = gsap.timeline({
   scrollTrigger: {
     trigger: "#terminal",
     start: "top center",
     end: "bottom bottom",
-  }
+    markers: true,
+  },
 });
 
-tlTerminal
-.add(TerminalDetails()); 
-
+tlTerminal.add(TerminalDetails());
 
 // const railItems = document.querySelectorAll(".rail-item");
 // const loop = horizontalLoop(railItems, { reversed: true, default: 5});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./css/**/*.{html,js}",
-    "./index.html",
-    "./script/**/*.{html,js}"
-  ],
+  darkMode: "selector",
+  content: ["./css/**/*.{html,js}", "./index.html", "./script/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#0d0f05",
+        background: "#fafbf4",
+        primary: "#49cd43",
+        secondary: "#84eb80",
+        accent: "55f34e",
+        textdark: "#f8faf0",
+        backgrounddark: "#100c14",
+        primarydark: "#e698b7",
+        secondarydark: "#8a1541",
+        accentdark: "#ee1769",
+      },
+      fontFamily: {
+        logo: ['"Megrim"', "sans-serif"],
+        body: ['"Oxanium"', "serif"],
+      },
+    },
+    //
   },
   plugins: [],
-}
-
+};
